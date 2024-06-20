83a9be888eb4221a1ef27c2c6c26d404a202cd53
Miquel Perelló Nieto <perello.nieto@gmail.com>
2024-06-20 20:02:00 +0100

    Changed structure - Added sections - Changed location of 2 pages

--- render.sh
+++ render.sh
@@ -3,26 +3,9 @@
 TO_RENDER=(index.qmd
            intro.qmd
            explainable-ai-systems.qmd
-           shinylive.qmd
-           cha_sl/shinylive.qmd
-           cha_sl/shinylive-not-working.qmd
            cha_wahcc/wahcc.qmd
-           cha_odm/odm.qmd
-           cha_ds/ds.qmd)
+           cha_odm/odm.qmd)
 
-echo "Removing previous slides"
-rm -rf slides
-echo "Loading virtual environment"
-source venv/bin/activate
-echo "Rendering Quarto slides"
-quarto render ${TO_RENDER[@]} --to revealjs 
-echo "Copying slides into a separate folder"
-cp -R _site slides
-echo "Rendering Quarto website"
-quarto render ${TO_RENDER[@]} --to html 
-echo "Removing slides folder from the website"
-rm -rf _site/slides
-echo "Copying slides into the website"
-cp -Rf slides _site/slides
-echo "Removing slides original copy"
-rm -rf slides
+quarto render ${TO_RENDER[@]} 
+
+
